14a0e942cddcbb8ff96929fd81ff0cfd6bf1cb6f
CodeVxDev <soberdev002@gmail.com>
2024-03-08 10:02:13 +0530

    Adding Postgres support+init script

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-    let _str = String::from("test3");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
+    let port = listner.local_addr().unwrap().port();
+    println!("{port}");
+    run(listner)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,76 @@
+use std::net::TcpListener;
+
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let response = client
+        .get(format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute the request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=Mitali%20Madhusmita&email=mitali.kunmun%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute the request.");
+
+    assert_eq!(200, response.status());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("Mitali%20Madhusmita", "Missing the email"),
+        ("", "mitali.kunmun%40gmail.com"),
+        ("", "Missing the email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute the request.");
+        assert_eq!(
+            400,
+            response.status(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
+}
+
+/// Spin up an instance of our application
+/// and returns its address (i.e. http://localhost:XXXX)
+fn spawn_app() -> String {
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
+    let port = listner.local_addr().unwrap().port();
+    let server = zero2prod::run(listner).expect("Failed to bind the server");
+    let _ = tokio::spawn(server);
+    format!("http://localhost:{}", port)
+}
